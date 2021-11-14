9125e86c2a6428621754a218923ec3f0c4382b4a
Claude Will <claude.will@yahoo.com>
2021-11-14 02:19:35 -0600

    more features added

--- flask_app/controllers/private.py
+++ flask_app/controllers/private.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, redirect, request, session
 import re
 from flask_bcrypt import Bcrypt
 from flask_app.models.user import User
+from flask_app.models.car import Car
 
 
 
@@ -13,9 +14,73 @@ def dashboard():
     data = {
         "id": session["user_id"]
     }
-    return render_template("dashboard.html", user = User.getSingleUser(data))
+    return render_template("dashboard.html", user = User.getSingleUser(data), all_cars = Car.getAllCars())
+
+
+@app.route("/add/<int:id>")
+def addCar(id):
+    user_id = session["user_id"]
+    userById = User.getSingleUser(id)
+
+    return render_template("add.html", user = userById)
+
+@app.route("/createCar/new",methods=["POST"])
+def createCar():
+    user_id = session["user_id"]
+    
+    if not Car.validateCar(request.form):
+        redirect(f"/add/{user_id}")
+    data = {
+        "model": request.form["model"],
+        "make": request.form["make"],
+        "description": request.form["description"],
+        "price": request.form["price"],
+        "year": request.form["year"],
+        "user_id": session["user_id"]
+    }
+    print(data)
+    
+        
+    Car.createCar(data)
+    return redirect("/dashboard")
+
+@app.route("/edit/<int:id>")
+def editSighting(id):
+    context = {
+        "user": User.getSingleUser(session["user_id"]),
+        "car": Car.getOneCarById(id)
+    }
+    return render_template("edit.html",**context)
+
+@app.route("/show/<int:car_id>")
+def show(car_id):
+    if 'user_id' not in session:
+        return redirect('/')
+    data = {
+        'id': id
+    }
+    
+    return render_template("show.html",car = Car.getOneWithUser(data), user = User.getSingleUser(data))
+
+
+@app.route("/car/<int:id>/update",methods=["POST"])
+def updateSighting(id):
+    if not Car.validateCar(request.form):
+        return redirect(f"/edit/{id}")
+    data = {
+        "id": id,
+        "price": request.form["price"],
+        "model": request.form["model"],
+        "make": request.form["make"],
+        "description": request.form["description"],
+        "year": request.form["year"],
+        "user_id": session["user_id"]
+    }
+    Car.updateSingleCar(data)
+    return redirect("/dashboard")
+
 
 @app.route("/delete/<int:id>")
 def deleteReport(id):
-    
+    Car.deleteCar(id)
     return redirect("/dashboard")
--- flask_app/models/car.py
+++ flask_app/models/car.py
@@ -3,8 +3,10 @@ from flask import flash
 import re
 from flask_app.models import user
 class Car:
+    db = "exam_2"
     def __init__(self,dbData) -> None:
         self.id = dbData["id"]
+        self.price = dbData["price"]
         self.model = dbData["model"]
         self.make = dbData["make"]
         self.year = dbData["year"]
@@ -12,9 +14,10 @@ class Car:
         self.created_at = dbData["created_at"]
         self.updated_at = dbData["updated_at"]
         self.user_id = dbData["user_id"]
+        self.user = None
 
     @classmethod
-    def createReport(cls,info):
+    def createCar(cls,info):
         query = "INSERT INTO cars (model,make,description,price,year,user_id) VALUES (%(model)s,%(make)s,%(description)s,%(price)s,%(year)s,%(user_id)s);"
         data = {
             "model": info["model"],
@@ -28,7 +31,7 @@ class Car:
         return new_car_id
     @classmethod
     def getAllCars(cls):
-        query = "SELECT * FROM cars AS sr LEFT JOIN users AS u ON u.id WHERE u.id = user_id;"
+        query = "SELECT * FROM cars AS c LEFT JOIN users AS u ON u.id WHERE u.id = user_id;"
         results = connectToMySQL(cls.db).query_db(query)
         all_cars = []
         
@@ -48,3 +51,82 @@ class Car:
             new_car.user = userD
             all_cars.append(new_car)
         return all_cars
+
+    @classmethod
+    def getOneCarById(cls,id):
+        q = "SELECT * FROM cars WHERE id = %(id)s;"
+        data = {
+            "id": id
+        }
+        print(id)
+        
+        res = connectToMySQL(cls.db).query_db(q,data)
+        if not res:
+            return False
+        return cls(res[0])
+
+    @classmethod
+    def getOneCar(cls,data):
+        query = "SELECT * FROM cars WHERE cars.id = %(id)s;"
+        print(id)
+        results = connectToMySQL(cls.db).query_db(query,data)
+        print(results)
+        if not results:
+            return False
+        return cls(results[0])
+    
+    @classmethod
+    def getOneWithUser(cls,data):
+        q = "SELECT * FROM cars AS c LEFT JOIN users AS u ON c.user_id = u.id WHERE c.id = %(id)s;"
+        r = connectToMySQL(cls.db).query_db(q,data)
+        print(r)
+        
+        data = {
+            "id": r[0]['user.id'], "first_name": r[0]['first_name'], "last_name": r[0]['last_name'], 'email': r[0]['email'],  'password': r[0]['password'], 'created_at': r[0]['created_at'], 'updated_at': r[0]['updated_at']
+        }
+        print(data)
+        car = cls(r[0])
+        car.user = user.User.getSingleUser(data)
+        return car
+
+
+    @classmethod
+    def updateSingleCar(cls,info):
+        query = "UPDATE cars SET model = %(model)s, make = %(make)s, description = %(description)s, price = %(price)s, year = %(year)s WHERE id = %(id)s;"
+        data = {
+            "id": info["id"],
+            "model": info["model"],
+            "make": info["make"],
+            "description": info["description"],
+            "price": info["price"],
+            "year": info["year"]
+        }
+        return connectToMySQL(cls.db).query_db(query,data)
+
+    @classmethod
+    def deleteCar(cls,id):
+        q = "DELETE FROM cars WHERE id = %(id)s;"
+        data = {
+            "id": id
+        }
+        return connectToMySQL(cls.db).query_db(q,data)
+
+    @staticmethod
+    def validateCar(data):
+        isValid = True
+        if len(data["price"]) == 0:
+            flash("The car must have a price")
+            isValid = False
+        if len(data["model"]) < 2:
+            flash("Model must be at least two characters long")
+            isValid = False
+        if len(data["make"]) < 2:
+            flash("Make must be at least two characters long")
+            isValid = False
+        if len(data["year"]) < 4:
+            flash("A four digit year is required")
+            isValid = False
+        if len(data["description"]) == 0:
+            flash("description is required")
+            isValid = False
+        return isValid
